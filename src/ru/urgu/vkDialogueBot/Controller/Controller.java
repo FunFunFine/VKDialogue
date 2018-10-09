e88ffbe781a2d328feb0b55469a377bac23ccf2d
Anton Voitsishevskii <grygry0@gmail.com>
2018-10-09 13:15:02 +0300

    Added observer pattern realisation to GUI;

--- src/ru/urgu/vkDialogueBot/Controller/Controller.java
+++ src/ru/urgu/vkDialogueBot/Controller/Controller.java
@@ -3,24 +3,33 @@ package ru.urgu.vkDialogueBot.Controller;
 import ru.urgu.vkDialogueBot.GUI.GUI;
 import ru.urgu.vkDialogueBot.Model.Model;
 
-public class Controller
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class Controller implements PropertyChangeListener
 {
     private final GUI _gui;
     private final Model _model;
     private final IMessenger _messenger;
-    private final EventManager _eventManager;
+
 
     public Controller(Model model, GUI gui)
     {
         _model = model;
         _messenger = new Messenger();
-
-        _eventManager = new EventManager(_messenger);
         _gui = gui;
+        _gui.addPropertyChangeListener(this);
     }
 
     public void startBot()
     {
         _gui.run();
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt)
+    {
+        var event = evt.getNewValue();
+        // do smth with event
     }
 }
--- src/ru/urgu/vkDialogueBot/GUI/GUI.java
+++ src/ru/urgu/vkDialogueBot/GUI/GUI.java
@@ -1,9 +1,43 @@
 package ru.urgu.vkDialogueBot.GUI;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
 public class GUI
 {
-    public void run()
+    private PropertyChangeSupport support;
+
+    public GUI()
     {
+        support = new PropertyChangeSupport(this);
 
     }
+    public void run()
+    {
+        while (true)
+        {
+            //run interface
+            // if (event)
+            //onEvent(event);
+        }
+
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener pcl)
+    {
+        support.addPropertyChangeListener(pcl);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener pcl)
+    {
+        support.removePropertyChangeListener(pcl);
+    }
+
+    private void onEvent()
+    {
+        support.firePropertyChange("Event", 1, 2);
+
+    }
+
 }
