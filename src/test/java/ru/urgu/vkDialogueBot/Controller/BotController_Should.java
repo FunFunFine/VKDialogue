131bbe9a9d2e5ebda6196849a2019271ed681c8d
GH2OOST <38152065+GH2OOST@users.noreply.github.com>
2018-12-18 00:13:47 +0500

    controller simple test

--- src/test/java/ru/urgu/vkDialogueBot/Controller/BotController_Should.java
+++ src/test/java/ru/urgu/vkDialogueBot/Controller/BotController_Should.java
@@ -1,44 +1,47 @@
 package ru.urgu.vkDialogueBot.Controller;
 
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import ru.urgu.vkDialogueBot.Controller.ObserverPattern.IObserver;
 import ru.urgu.vkDialogueBot.Events.Signal;
+import ru.urgu.vkDialogueBot.Events.UserIOSignal;
+import ru.urgu.vkDialogueBot.Model.IVkModel;
 import ru.urgu.vkDialogueBot.View.IView;
 
+import static org.mockito.Mockito.*;
+import org.junit.Test;
+
+@RunWith(MockitoJUnitRunner.class)
 public class BotController_Should
 {
+    IView fakeView;
+    IVkModel fakeModel;
+    BotController controller;
 
-}
 
-class FakeGui implements IView
-{
-
-    @Override
-    public void run()
+    @Before
+    public void setUp()
     {
-
+        fakeView = Mockito.mock(IView.class);
+        fakeModel = mock(IVkModel.class);
+        var dataBase = new UsersDataBase();
+        controller = new BotController(fakeModel, fakeView, dataBase);
     }
 
-    @Override
-    public void notify(Signal event)
+    @Test
+    public void Run_Gui()
     {
-
+        controller.runBot();
+        verify(fakeView).run();
     }
 
-    @Override
-    public void addObserver(IObserver observer)
+    @Test
+    public void SendMessages()
     {
-
+        var signal = new UserIOSignal("отправить 1234");
+        //controller.
     }
 
-    @Override
-    public void removeObserver(IObserver observer)
-    {
-
-    }
-
-    @Override
-    public void receive(Signal event)
-    {
-
-    }
 }
